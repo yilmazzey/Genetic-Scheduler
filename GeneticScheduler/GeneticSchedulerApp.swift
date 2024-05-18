236cbc43b7062bec562c212038caa8a7b5d6c4c6
yilmazzey <zeynepyilmaz0235@gmail.com>
2024-05-18 14:09:36 +0300

    Initial Commit

---
+++ GeneticScheduler/GeneticSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeneticSchedulerApp.swift
+//  GeneticScheduler
+//
+//  Created by Zeynep Yılmaz on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeneticSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
